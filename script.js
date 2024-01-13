e4e8d46e9f6fc2454d1aa29d8aecd2e4f042912d
Debasishkaity22 <debasishkaity22@gmail.com>
2024-01-13 23:54:49 +0530

    my project

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-console.log("hi");
-let hourText=document.querySelector(".hours-text");
-    let minuteText=document.querySelector(".minutes-text");
-    let secondsText=document.querySelector(".seconds-text");
+let timerId;
 let timers = [];
+let form=document.querySelector("form");
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    startNewTimer();
+    form.reset();
+})
 function startNewTimer(){
     const hours = parseInt(document.getElementById('hours').value) || 0;
     const minutes = parseInt(document.getElementById('minutes').value) || 0;
@@ -17,7 +20,7 @@ function startNewTimer(){
     let totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
     
 
-    let timerId = setInterval(() => {
+     timerId = setInterval(() => {
         if (totalTimeInSeconds <= 0) {
             clearInterval(timerId);
             handleTimerEnd(timerId);
@@ -30,25 +33,25 @@ function startNewTimer(){
 
     timers.push({ id: timerId, totalTimeInSeconds });
     updateActiveTimersDisplay();
-    console.log(hours,minutes,seconds,totalTimeInSeconds,timers);
 }
 function updateTimerDisplay(timerId, remainingTime) {
     const timerElement = document.getElementById(timerId);
     const hours = Math.floor(remainingTime / 3600);
     const minutes = Math.floor((remainingTime % 3600) / 60);
     const seconds = remainingTime % 60;
-    
-    console.log(hourText,minuteText,secondsText);
-    hourText.innerText=formatTime(hours);
-    minuteText.innerText=formatTime(minutes);
+    let hourText=document.querySelector(".hours-text");
+    let minuteText=document.querySelector(".minutes-text");
+    let secondsText=document.querySelector(".seconds-text");
+    hourText.innerText=formatTime(hours)+":";
+    minuteText.innerText=formatTime(minutes)+":";
     secondsText.innerText=formatTime(seconds);
-    // timerElement.innerHTML = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
 }
 function handleTimerEnd(timerId) {
     const timerIndex = timers.findIndex(timer => timer.id === timerId);
     timers.splice(timerIndex, 1);
 
     updateActiveTimersDisplay();
+    playAudioAlert();
     const endedTimerElement = document.getElementById(timerId);
     endedTimerElement.classList.add('timer-end');
 }
@@ -61,18 +64,45 @@ function updateActiveTimersDisplay() {
         let timerElement1 = document.createElement('div');
         let timerElement2 = document.createElement('div');
         timerElement.classList.add('timer-task');
-        timerElement1.classList.add("set-time")
+        timerElement1.classList.add("set-time");
+        timerElement1.innerText="Time Left : ";
+        timerElement2.classList.add("timer-count");
+        let para1 = document.createElement('p');
+        let para2 = document.createElement('p');
+        let para3 = document.createElement('p');
+        para1.classList.add("hours-text");
+        para2.classList.add("minutes-text");
+        para3.classList.add("seconds-text");
+        timerElement2.append(para1,para2,para3);
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerText="Delete";
+        deleteBtn.classList.add("btn");
         timerElement.id = timer.id;
+        timerElement.append(timerElement1,timerElement2,deleteBtn);
+        let curentTimer1=document.querySelector(".curent-timer");
+        curentTimer1.appendChild(timerElement);
         updateTimerDisplay(timer.id, timer.totalTimeInSeconds);
 
         const stopButton = document.createElement('button');
-        stopButton.textContent = 'Stop Timer';
-        stopButton.onclick = () => handleTimerEnd(timer.id);
-
-        timerElement.appendChild(stopButton);
-        activeTimersSection.appendChild(timerElement);
+        stopButton.textContent = 'Stop audio';
+        // deleteBtn.onclick = () => handleTimerEnd(timer.id);
+        timerElement2.appendChild(stopButton);
+        curentTimer1.appendChild(timerElement);
+        stopButton.addEventListener("click",function(){
+            audio.pause();
+        })
+        deleteBtn.addEventListener("click",function(){
+            audio.pause();
+            clearInterval(timerId);
+            timerElement.remove();
+        });
     });
 }
 function formatTime(time) {
     return time < 10 ? `0${time}` : `${time}`;
+}
+const audio = new Audio('./audio/zero-action-trailer-teaser-146679.mp3');
+function playAudioAlert() {
+   
+    audio.play();
 }
